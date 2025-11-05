fee26c6f0934823796130625adf16b5971d651b9
Keren Gantman <keren.gantman@post.runi.ac.il>
2025-11-05 14:41:55 +0200

    finishFVC2

--- FVCalc.java
+++ FVCalc.java
@@ -4,8 +4,8 @@ public class FVCalc {
 		double currentValue= Double.parseDouble(args[0]); 
 		double interest= Double.parseDouble(args[1]);
 		double years= Double.parseDouble(args[2]);
-		double futureValue= (double) (currentValue * Math.pow((1 + interest / 100.0), years));
-		System.out.println( "After"  + (int)years+  " years," +(int)currentValue+ "$" + "saved at " +(int)interest+ "%" + "will yield" + (int)futureValue+ "$");
+		int futureValue= (int) (currentValue * Math.pow((1 + interest / 100.0), years));
+		System.out.println("After " + (int)years +  " years, " + (int)currentValue + "$" + " saved at " + (int)interest + "%" + " will yield " + (int)futureValue+ "$");
 
 	}
 }
